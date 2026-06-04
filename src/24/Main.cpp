925c89c34591532bf2a0ee68c2464de230cbc8c8
Seojin Kim <rlatjwls7882@gmail.com>
2026-06-04 17:13:02 +0900

    Create Main.cpp

---
+++ src/24/Main.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long preSum[100'000];
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, q; cin >> n >> q;
+    for(int i=1;i<=n;i++) {
+        cin >> preSum[i];
+        preSum[i]+=preSum[i-1];
+    }
+    while(q--) {
+        int l, r; cin >> l >> r;
+        cout << preSum[r]-preSum[l-1] << '\n';
+    }
+}
