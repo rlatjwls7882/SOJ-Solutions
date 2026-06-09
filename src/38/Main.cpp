59cfee587ea9540e06e10697156ec89bb53c8e41
Seojin Kim <rlatjwls7882@gmail.com>
2026-06-09 19:31:41 +0900

    Create Main.cpp

---
+++ src/38/Main.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+const ll LINF=0x3f3f3f3f3f3f3f3f;
+
+struct element {
+    ll u, w;
+    bool operator<(const element e) const {
+        return w > e.w;
+    }
+};
+
+ll dist[100'001];
+vector<vector<element>> conn(100'001);
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m, s; cin >> n >> m >> s;
+    while(m--) {
+        int u, v, w; cin >> u >> v >> w;
+        conn[u].push_back({v, w});
+        conn[v].push_back({u, w});
+    }
+
+    fill(dist, dist+n+1, LINF);
+    priority_queue<element> pq; pq.push({s, 0});
+    while(!pq.empty()) {
+        auto [cur, cost] = pq.top(); pq.pop();
+        if(dist[cur]<=cost) continue;
+        dist[cur]=cost;
+        for(auto [nxt, co]:conn[cur]) {
+            if(dist[nxt]>dist[cur]+co) {
+                pq.push({nxt, dist[cur]+co});
+            }
+        }
+    }
+    for(int i=1;i<=n;i++) cout << (dist[i]==LINF ? -1 : dist[i]) << "\n";
+}
