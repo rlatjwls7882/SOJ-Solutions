bdc97eb3311a57537cb742cd481e7d12e6bc0dfe
Seojin Kim <rlatjwls7882@gmail.com>
2026-06-11 08:08:30 +0900

    Create Main.cpp

---
+++ src/43/Main.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int a[1001], b[1001], vis[1001];
+vector<vector<int>> conn(1001);
+
+bool dfs(int cur) {
+    vis[cur]=true;
+    for(int next:conn[cur]) {
+        if(b[next]==-1 || !vis[b[next]] && dfs(b[next])) {
+            a[cur]=next;
+            b[next]=cur;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m, k; cin >> n >> m >> k;
+    while(k--) {
+        int a, b; cin >> a >> b;
+        conn[a].push_back(b);
+    }
+
+    int cnt=0;
+    memset(b, -1, sizeof b);
+    for(int i=1;i<=n;i++) {
+        memset(vis, 0, sizeof vis);
+        cnt+=dfs(i);
+    }
+    cout << cnt;
+}
