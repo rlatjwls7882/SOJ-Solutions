ce7972adda1ac83ea4308ac4fe189208a89e3b3c
Seojin Kim <rlatjwls7882@gmail.com>
2026-06-11 06:24:36 +0900

    Create Main.cpp

---
+++ src/41/Main.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int inD[100'001];
+vector<vector<int>> conn(100'001);
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int n, m; cin >> n >> m;
+    while(m--) {
+        int u, v; cin >> u >> v;
+        conn[u].push_back(v);
+        inD[v]++;
+    }
+
+    vector<int> res;
+    queue<int> q;
+    for(int i=1;i<=n;i++) {
+        if(!inD[i]) {
+            q.push(i);
+            res.push_back(i);
+        }
+    }
+    while(!q.empty()) {
+        int cur=q.front(); q.pop();
+        for(int next:conn[cur]) {
+            if(--inD[next]==0) {
+                res.push_back(next);
+                q.push(next);
+            }
+        }
+    }
+    if(res.size()!=n) cout << "CYCLE";
+    else for(auto e:res) cout << e << ' ';
+}
